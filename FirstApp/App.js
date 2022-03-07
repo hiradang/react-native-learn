459d218df5d619bc1af7f7e1e8c5506788eb2a87
hiradang <sunnybinhdang@gmail.com>
2022-03-07 21:22:20 +0700

    lesson 14 Modal & Customize Alert

--- FirstApp/App.js
+++ FirstApp/App.js
@@ -14,11 +14,8 @@ import {
   TextInput,
   Keyboard,
   TouchableWithoutFeedback,
-  TouchableOpacity,
-  Button,
+  Modal,
   Pressable,
-  Alert,
-  ToastAndroid,
 } from 'react-native';
 
 const DismissKeyboard = ({children}) => (
@@ -30,63 +27,42 @@ const DismissKeyboard = ({children}) => (
 const App = () => {
   const [name, setName] = useState('');
   const [submit, setSubmit] = useState(false);
+  const [showWarning, setShowWarning] = useState(false);
 
   const onSubmit = () => {
     if (name.length > 3) {
       setSubmit(!submit);
     } else {
-      Alert.alert(
-        'warning',
-        'The name must be at least 3 characters',
-        [
-          // List of buttons
-          // Android: maximum 3
-          // IOS: infinitive
-          {
-            text: 'Cancel',
-            onPress: () => console.warn('Cancel Pressed'),
-            style: 'destructive',
-          },
-          {
-            text: 'OK',
-            onPress: () => console.warn('OK Pressed'),
-            style: 'destructive',
-          },
-        ],
-        // dismiss by tapping outside the alert
-        {
-          cancelable: true,
-          onDismiss: () => {
-            console.warn('Do something when the alert close');
-          },
-        },
-      );
-
-      ToastAndroid.show(
-        'The name must be at least 3 characters',
-        ToastAndroid.SHORT,
-        // default location: bottom
-      );
-
-      // ToastAndroid.showWithGravity(
-      //   'The name must be at least 3 characters',
-      //   ToastAndroid.LONG,
-      //   ToastAndroid.CENTER,
-      // );
-
-      // ToastAndroid.showWithGravityAndOffset(
-      //   'The name must be at least 3 characters',
-      //   ToastAndroid.SHORT,
-      //   ToastAndroid.TOP,
-      //   200,
-      //   0,
-      // );
+      setShowWarning(true);
     }
   };
 
   return (
     <DismissKeyboard>
       <View style={styles.body}>
+        <Modal
+          visible={showWarning}
+          onRequestClose={() => setShowWarning(false)}
+          transparent={true}>
+          <View style={styles.warningContainer}>
+            <View style={styles.warningModal}>
+              <View style={styles.warningHeader}>
+                <Text>WARNING!</Text>
+              </View>
+
+              <View style={styles.warningBody}>
+                <Text>Your name must be at least 3 characters long.</Text>
+              </View>
+
+              <Pressable
+                onPress={() => setShowWarning(false)}
+                style={styles.warningButton}
+                android_ripple={{color: '#fff'}}>
+                <Text style={styles.text}>OK</Text>
+              </Pressable>
+            </View>
+          </View>
+        </Modal>
         <Text style={styles.text}>Please tell me you name:</Text>
         <TextInput
           // allow to input in many lines
@@ -99,15 +75,6 @@ const App = () => {
           maxLength={20}
           // secureTextEntry
         />
-
-        {/* <Button title={submit ? 'Clear' : 'Submit'} onPress={onSubmit}></Button> */}
-
-        {/* <TouchableOpacity
-          style={styles.button}
-          onPress={onSubmit}
-          activeOpacity={0.7}>
-          <Text>{submit ? 'Clear' : 'Submit'}</Text>
-        </TouchableOpacity> */}
 
         <Pressable
           onPress={onSubmit}
@@ -153,6 +120,39 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     marginTop: 20,
+  },
+  warningContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#00000099',
+  },
+  warningModal: {
+    backgroundColor: '#fff',
+    width: 300,
+    height: 300,
+    borderRadius: 20,
+    borderWidth: 1,
+    borderColor: '#000',
+  },
+  warningHeader: {
+    height: 50,
+    backgroundColor: '#fa4866',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderTopRightRadius: 20,
+    borderTopLeftRadius: 20,
+  },
+  warningBody: {
+    height: 200,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  warningButton: {
+    backgroundColor: '#42f566',
+    borderBottomLeftRadius: 20,
+    borderBottomRightRadius: 20,
+    height: 50,
   },
 });
 
