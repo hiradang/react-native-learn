14a984fe5503e677cafec9eeb0ef609b057398ce
hiradang <sunnybinhdang@gmail.com>
2022-03-19 15:59:02 +0700

    lesson 27 Fetch Data from an API

--- FirstApp/src/redux/actions.js
+++ FirstApp/src/redux/actions.js
@@ -1,6 +1,35 @@
 export const SET_USER_NAME = 'SET_USER_NAME';
 export const SET_USER_AGE = 'SET_UESR_AGE';
 export const INCREASE_AGE = 'INCREASE_AGE';
+export const GET_CITIES = 'GET_CITIES';
+
+const API_URL = 'https://mocki.io/v1/22ee83d9-d06c-4867-afe2-30cc3bfdeb13';
+
+export const getCities = () => {
+  try {
+    return async dispatch => {
+      const result = await fetch(API_URL, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      const cities = await result.json();
+
+      if (cities) {
+        dispatch({
+          type: 'GET_CITIES',
+          payload: cities,
+        });
+      } else {
+        console.log('Unable to fetch!');
+      }
+    };
+  } catch (err) {
+    console.error(err);
+  }
+};
 
 export const setName = name => dispatch => {
   dispatch({
--- FirstApp/src/screens/Home.js
+++ FirstApp/src/screens/Home.js
@@ -6,13 +6,13 @@ import {
   Pressable,
   Alert,
   TextInput,
+  FlatList,
 } from 'react-native';
 import GlobalStyle from '../utils/GlobalStyle';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 import CustomButton from '../utils/CustomButton';
 import SQLite from 'react-native-sqlite-storage';
 import {useSelector, useDispatch} from 'react-redux';
-import {setName, setAge, increaseAge} from '../redux/actions';
+import {setName, setAge, increaseAge, getCities} from '../redux/actions';
 
 const db = SQLite.openDatabase(
   {
@@ -26,7 +26,7 @@ const db = SQLite.openDatabase(
 );
 
 function Home({navigation, route}) {
-  const {name, age} = useSelector(state => state.userReducer);
+  const {name, age, cities} = useSelector(state => state.userReducer);
   const dispatch = useDispatch();
 
   // const [name, setName] = useState('');
@@ -34,6 +34,7 @@ function Home({navigation, route}) {
 
   useEffect(() => {
     getData();
+    dispatch(getCities());
   }, []);
 
   const getData = () => {
@@ -100,22 +101,16 @@ function Home({navigation, route}) {
     <View style={styles.body}>
       <Text style={[GlobalStyle.GlobalStyle, styles.text]}>Home </Text>
       <Text style={[GlobalStyle.GlobalStyle, styles.text]}>Welcome {name}</Text>
-      <Text style={[GlobalStyle.GlobalStyle, styles.text]}>
-        Your age is: {age}
-      </Text>
 
-      <TextInput
-        style={styles.input}
-        placeholder="Enter your name!"
-        value={name}
-        onChangeText={value => dispatch(setName(value))}></TextInput>
-
-      <CustomButton title="Update" color="#32a852" pressHandler={updateData} />
-      <CustomButton title="Remove" color="#f55" pressHandler={removeData} />
-      <CustomButton
-        title="Increase Age"
-        color="#0080ff"
-        pressHandler={() => dispatch(increaseAge())}
+      <FlatList
+        data={cities}
+        renderItem={({item}) => (
+          <View style={styles.item}>
+            <Text style={styles.title}>{item.country}</Text>
+            <Text style={styles.subtitle}>{item.city}</Text>
+          </View>
+        )}
+        keyExtractor={(item, index) => index.toString()}
       />
     </View>
   );
@@ -143,6 +138,25 @@ const styles = StyleSheet.create({
     fontSize: 20,
     marginTop: 130,
     marginBottom: 10,
+  },
+  item: {
+    backgroundColor: '#ffffff',
+    borderWidth: 2,
+    borderColor: '#cccccc',
+    borderRadius: 5,
+    margin: 7,
+    width: 350,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 30,
+    margin: 10,
+  },
+  subtitle: {
+    fontSize: 20,
+    margin: 10,
+    color: '#999999',
   },
 });
 
